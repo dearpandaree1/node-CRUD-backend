143c20a6ca01372cfc90b0cae0540dc50a6776df
pandaree <pandaree.c51@gmail.com>
2024-05-18 18:19:33 +0700

    finish backend

--- src/routes/address-route.js
+++ src/routes/address-route.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const addressesController = require('../controller/address-controller')
+const express = require("express");
+const addressesController = require("../controller/address-controller");
 const router = express.Router();
+const multer = require("multer");
+
+const upload = multer();
 
 router.get("/", addressesController.getAddress);
 router.get("/:addressId", addressesController.getAddressById);
-router.post("/", addressesController.createAddress);
+router.post("/", upload.none(), addressesController.createAddress);
 router.delete("/:addressId", addressesController.deleteAddress);
-router.patch("/:addressId", addressesController.editAddress);
+router.patch("/:addressId", upload.none(), addressesController.editAddress);
 
 module.exports = router;
-
